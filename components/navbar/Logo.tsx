578ccbe4fcdf723e48ecfe00d6ddd275d14ba7fd
eviatarHasid <evi1020@gmail.com>
2024-08-27 16:47:29 -0400

    added logo and search line to nav bar

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,4 +1,14 @@
+import { GiWoodCabin } from "react-icons/gi";
+import { Button } from "../ui/button";
+import Link from "next/link";
+
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button size="icon" asChild>
+      <Link href="/">
+        <GiWoodCabin className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 }
 export default Logo;
